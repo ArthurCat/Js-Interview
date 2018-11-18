8d05541a610bc92e54619b5cc040ffec24de8571
ArthurCat <arthurzheng9@gmail.com>
2018-11-18 00:00:10 -0500

    Create 160. intersection of two linked lists.js

---
+++ Algorithm/Leetcode/160. intersection of two linked lists.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+  if (!headA || !headB) {
+    return null;
+  }
+  let pa = headA;
+  let pb = headB;
+  while (pa !== pb) {
+    if (!pa) {
+      pa = headB;
+    } else {
+      pa = pa.next;
+    }
+    if (!pb) {
+      pb = headA;
+    } else {
+      pb = pb.next;
+    }
+  }
+  return pa;
+};

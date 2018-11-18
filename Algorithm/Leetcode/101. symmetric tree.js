a1a657fac5ccb7ab485c941aef19b4b84f8fec17
ArthurCat <arthurzheng9@gmail.com>
2018-11-17 22:16:20 -0500

    Create 101. symmetric tree.js

---
+++ Algorithm/Leetcode/101. symmetric tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+  if (!root) {
+    return true;
+  }
+  function dfs(m, n) {
+    if (!m && !n) {
+      return true;
+    }
+    if ((!m && n) || (m && !n) || (m.val !== n.val)) {
+      return false;
+    }
+    return dfs(m.left, n.right) && dfs(m.right, n.left);
+  }
+  return dfs(root.left, root.right);
+};

7a2dfa314df579afba93994f08cc0a00839f3122
ArthurCat <arthurzheng9@gmail.com>
2018-11-21 02:21:38 -0500

    Create 448. find all numbers disappeared in an array.js

---
+++ Algorithm/Leetcode/448. find all numbers disappeared in an array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+var findDisappearedNumbers = function(nums) {
+  let result = [];
+  
+  for (let num of nums) {
+    let val = Math.abs(num) - 1;
+    if (nums[val] > 0) {
+      nums[val] = -nums[val];
+    }
+  }
+  
+  nums.forEach((elem, index) => {
+    if(elem > 0) {
+      result.push(index + 1);
+    }
+  })
+  return result;
+};
+

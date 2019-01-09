82890d3fbf4475a870d5c2f0be9e765bf312b9ad
ArthurCat <arthurzheng9@gmail.com>
2019-01-09 16:59:12 -0500

    Update 064. minimum path sum.js

--- Algorithm/Leetcode/064. minimum path sum.js
+++ Algorithm/Leetcode/064. minimum path sum.js
@@ -19,3 +19,20 @@ var minPathSum = function(grid) {
   }
   return grid[m - 1][n -1];
 };
+//optimized O(n^2) O(min(m, n)
+var minPathSum = function(grid) {
+  let m = grid.length;
+  let n = grid[0].length;
+  let cur = Array(m).fill(grid[0][0]);
+  
+  for (let i = 1; i < m; i++) {
+    cur[i] = cur[i - 1] + grid[i][0];
+  }
+  for (let j = 1; j < n; j++) {
+    cur[0] += grid[0][j];
+    for (let i = 1; i < m; i++){
+      cur[i] = Math.min(cur[i - 1], cur[i]) + grid[i][j];
+    }
+  }
+  return cur[m - 1];
+};

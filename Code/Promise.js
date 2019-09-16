ceec459d1443a700b5a36a10f69a6e4755ce2142
ArthurCat <arthurzheng9@gmail.com>
2019-09-15 23:19:14 -0400

    Update Promise.js

--- Code/Promise.js
+++ Code/Promise.js
@@ -1,4 +1,4 @@
-//Ajax with Promise
+//Ajax with Promise 'GET'
 const gstJSON = (url) => {
   const promise = new Promise((resolve, reject) => {
     const handler = () {
@@ -30,8 +30,32 @@ getJSON('/posts.json').then(
   (error) => {
     console.log('Error!', error);
   });
+//Ajax 'POST'
+const clien = XMLHttpRequest();
+client.open('post', url);
+client.setRequestHeader('contentType','application/json');
+client.send(JSON.stringfy({name:"Zheng", id:"123"});
   
-  
+//promise to async
+function getProcessedData(url) {
+  return downloadData(url) // returns a promise
+    .catch(e => {
+      return downloadFallbackData(url); // returns a promise
+    })
+    .then(v => {
+      return processDataInWorker(v); // returns a promise
+    });
+}
+
+async function getProcessedData(url) {
+  let v;
+  try {
+    v = await downloadData(url); 
+  } catch(e) {
+    v = await downloadFallbackData(url);
+  }
+  return processDataInWorker(v);
+}
 
 //Load Image
 const loadImageAsync(url) {
